48254f5203e7285af7a57b421f3e27b3834e18d2
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-02-18 17:27:14 +0100

    upload test

--- imageUploads/apps.py
+++ imageUploads/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class ImageuploadsConfig(AppConfig):
+class ImageUploadsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'imageUploads'
+
